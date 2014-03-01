95a429d2f9327a54ec4bce06f13733897bd02dc5
Zachary Clapper <zachary@alejandro.(none)>
2014-02-28 19:01:07 -0500

    Initial commit

---
+++ php/dbconfig.example.php
@@ -0,0 +1,14 @@
+<?php
+//
+// This is an example config file, the real one should be named "dbconfig.php"
+//
+
+// MySQL Credentials
+$db_host = "localhost";
+$db_user = "username";     // Enter your MySQL username
+$db_pass = "password";     // Enter your MySQL password
+$db_name = "torque";
+$db_table = "raw_logs";
+
+?>
+
